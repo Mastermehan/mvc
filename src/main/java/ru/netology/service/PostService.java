5589c8eac1937e008953d0e6415e294fd211562f
Viktor Loskutov <vikandloskutov@gmail.com>
2022-02-10 22:58:44 +0300

    Initial commit

---
+++ src/main/java/ru/netology/service/PostService.java
@@ -0,0 +1,42 @@
+package ru.netology.service;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.server.ResponseStatusException;
+import ru.netology.exception.NotFoundException;
+import ru.netology.model.Post;
+import ru.netology.repository.PostRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@ResponseStatus(code = HttpStatus.NOT_FOUND)
+public class PostService {
+  private final PostRepository repository;
+
+  public PostService(PostRepository repository) {
+    this.repository = repository;
+  }
+
+  public List<Post> all() {
+    return repository.all();
+  }
+
+  public Post getById(long id) {
+    Optional<Post> output = repository.getById(id);
+    if(output.isPresent()){
+      return output.get();
+    }
+    throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Post Not Found");
+  }
+
+  public Post save(Post post) {
+    return repository.save(post);
+  }
+
+  public void removeById(long id) {
+    repository.removeById(id);
+  }
+}
